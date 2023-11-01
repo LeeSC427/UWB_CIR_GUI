1d7435922bce22304a0ce524114f2b5b85e69795
LeeSC427 <sohwa427@gmail.com>
2023-11-01 18:15:07 +0900

    chore(first_version): first version of pozyx, need to test

--- workspace/admm.py
+++ workspace/admm.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python
 
 import numpy as np
-from calculation import Calculation
+from cal import CAL
 
-class TAG:
-    def __init__(self, Calculation):
-        self.tag_comp1   = np.array([0,0]).reshape(2,1)
-        self.tag_comp2   = np.array([0,0]).reshape(2,1)
-        self.tag_compAP1 = np.array([0,0]).reshape(2,1)
-        self.tag_compAP2 = np.array([0,0]).reshape(2,1)
-        self.tag_compAP3 = np.array([0,0]).reshape(2,1)
-        self.tag_compAP4 = np.array([0,0]).reshape(2,1)
+class ADMM:
+    def __init__(self):
+        self.tag_comp_1   = np.array([0,0]).reshape(2,1)
+        self.tag_comp_2   = np.array([0,0]).reshape(2,1)
+        self.tag_comp_AP1 = np.array([0,0]).reshape(2,1)
+        self.tag_comp_AP2 = np.array([0,0]).reshape(2,1)
+        self.tag_comp_AP3 = np.array([0,0]).reshape(2,1)
+        self.tag_comp_AP4 = np.array([0,0]).reshape(2,1)
 
         self.p = 90
 
-        self.tag_a
-        self.tag_b
-        self.tag_ap1
-        self.tag_ap2
-        self.tag_ap3
-        self.tag_ap4
+        self.tag_predict
+        self.tag_comp
 
-    def admm(self, est1, est2, est3, ap1, ap2, ap3, ap4, d):
-        [tag_x1, tag_y1, tag_x2, tag_y2, tag_ap1x, tag_ap1y, tag_ap2x, tag_ap2y, tag_ap3x, tag_ap3y, tag_ap4x, tag_ap4y] = Calculation.cal_diff(est1,est2,est3,ap1,ap2,ap3,ap4)
-        
-        tag_a_d = self.tag_a[0] + np.random.randn(1) * Calculation.sigma_d
-        tag_b_d = self.tag_b[0] + np.random.randn(1) * Calculation.sigma_d
+        self.calculator = CAL()
 
-        [tag_predic1  ,tag_comp1  ] = Calculation.predic_comp(tag_x1,   tag_y1,   self.p, Calculation.sigma_d, est1, tag_comp1,   d[4])
-        [tag_predic2  ,tag_comp2  ] = Calculation.predic_comp(tag_x2,   tag_y2,   self.p, Calculation.sigma_d, est1, tag_comp2,   d[5])
-        [tag_predicAP1,tag_compAP1] = Calculation.predic_comp(tag_ap1x, tag_ap1y, self.p, Calculation.sigma_d, est1, tag_compAP1, d[0])
-        [tag_predicAP2,tag_compAP2] = Calculation.predic_comp(tag_ap2x, tag_ap2y, self.p, Calculation.sigma_d, est1, tag_compAP2, d[1])
-        [tag_predicAP3,tag_compAP3] = Calculation.predic_comp(tag_ap3x, tag_ap3y, self.p, Calculation.sigma_d, est1, tag_compAP3, d[2])
-        [tag_predicAP4,tag_compAP4] = Calculation.predic_comp(tag_ap4x, tag_ap4y, self.p, Calculation.sigma_d, est1, tag_compAP4, d[3])
+    def admm(self, tag_a, tag_b, est_1, est_2, est_3, ap_1, ap_2, ap_3, ap_4, d):
+        [tag_x1    , tag_y1    ] = self.calculator.diff(est_1, est_2)
+        [tag_x2    , tag_y2    ] = self.calculator.diff(est_1, est_3)
+        [tag_ap_x_1, tag_ap_y_1] = self.calculator.diff(est_1, ap_1 )
+        [tag_ap_x_2, tag_ap_y_2] = self.calculator.diff(est_1, ap_2 )
+        [tag_ap_x_3, tag_ap_y_3] = self.calculator.diff(est_1, ap_3 )
+        [tag_ap_x_4, tag_ap_y_4] = self.calculator.diff(est_1, ap_4 )
 
-        tag_predic = tag_predic1 + tag_predic2 + tag_predicAP1 + tag_predicAP2 + tag_predicAP3 + tag_predicAP4
-        tag_comp = tag_comp1+tag_comp2+tag_compAP1+tag_compAP2+tag_compAP3+tag_compAP4
+        #tag_a_d = self.tag_a[0] + np.random.randn(1) * self.calculator.sigma_d
+        #tag_b_d = self.tag_b[0] + np.random.randn(1) * self.calculator.sigma_d
 
-        return [tag_predic, tag_comp]
+        [tag_predic1  ,tag_comp1  ] = self.calculator.predic_n_comp(tag_x1    , tag_y1    , self.p, self.calculator.sigma_d, est_1, tag_comp1  , d[4])
+        [tag_predic2  ,tag_comp2  ] = self.calculator.predic_n_comp(tag_x2    , tag_y2    , self.p, self.calculator.sigma_d, est_1, tag_comp2  , d[5])
+        [tag_predicAP1,tag_compAP1] = self.calculator.predic_n_comp(tag_ap_x_1, tag_ap_y_1, self.p, self.calculator.sigma_d, est_1, tag_compAP1, d[0])
+        [tag_predicAP2,tag_compAP2] = self.calculator.predic_n_comp(tag_ap_x_2, tag_ap_y_2, self.p, self.calculator.sigma_d, est_1, tag_compAP2, d[1])
+        [tag_predicAP3,tag_compAP3] = self.calculator.predic_n_comp(tag_ap_x_3, tag_ap_y_3, self.p, self.calculator.sigma_d, est_1, tag_compAP3, d[2])
+        [tag_predicAP4,tag_compAP4] = self.calculator.predic_n_comp(tag_ap_x_4, tag_ap_y_4, self.p, self.calculator.sigma_d, est_1, tag_compAP4, d[3])
+
+        self.tag_predict = tag_predic1 + tag_predic2 + tag_predicAP1 + tag_predicAP2 + tag_predicAP3 + tag_predicAP4
+        self.tag_comp    = tag_comp1 + tag_comp2 + tag_compAP1 + tag_compAP2 + tag_compAP3 + tag_compAP4
